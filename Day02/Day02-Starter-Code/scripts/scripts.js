edfc45f6270ebc4af45e0eb30d670f876410d503
archangel205 <askeeter84@gmail.com>
2019-02-17 15:25:44 -0600

    inputTags added

--- Day02/Day02-Starter-Code/scripts/scripts.js
+++ Day02/Day02-Starter-Code/scripts/scripts.js
@@ -1,3 +1,12 @@
+let inputTags = document.getElementsByTagName("input");
+for(let i = 0; i < inputTags.length; i++){
+    inputTags[i].addEventListener("focus", function(){
+        this.style.backgroundColor = "lightyellow";
+    }, false);
+    inputTags[i].addEventListener("blur", function(){
+        this.style.backgroundColor = "";
+    }, false);
+};		
 let pTags = document.getElementsByTagName("p");
 for(let i = 0; i < pTags.length; i++){
     pTags[i].addEventListener("mouseover", function(){

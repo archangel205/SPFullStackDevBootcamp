6c9ad5b403d566e27d5348a648fd3c53adaf817a
archangel205 <askeeter84@gmail.com>
2019-02-17 18:44:44 -0600

    added validate form function

--- Day02/Day02-Starter-Code/scripts/scripts.js
+++ Day02/Day02-Starter-Code/scripts/scripts.js
@@ -1,3 +1,18 @@
+function validateForm(){
+    let empName = document.forms["frmCollectWeights"]["empName"];
+    let empWeight = document.forms["frmCollectWeights"]["empWeight"];
+    if(empName.value == ""){
+        document.getElementById("nameMessage").innerHTML="Name cannot be empty!";
+        empName.focus();
+        return false;
+    }
+    if(empWeight.value == ""){
+        document.getElementById("weightMessage").innerHTML="Weight cannot be empty!";
+        empWeight.focus();
+        return false;
+    }
+    return true;
+}
 let inputTags = document.getElementsByTagName("input");
 for(let i = 0; i < inputTags.length; i++){
     inputTags[i].addEventListener("focus", function(){
@@ -6,7 +21,8 @@ for(let i = 0; i < inputTags.length; i++){
     inputTags[i].addEventListener("blur", function(){
         this.style.backgroundColor = "";
     }, false);
-};		
+};
+//		
 let pTags = document.getElementsByTagName("p");
 for(let i = 0; i < pTags.length; i++){
     pTags[i].addEventListener("mouseover", function(){

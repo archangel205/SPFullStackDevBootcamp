09b37f985d181b4a3a32bf331c1fc7a8078ed25c
archangel205 <askeeter84@gmail.com>
2019-02-26 20:52:32 -0600

    emp weight validation

--- Day02/Day02-Starter-Code/scripts/scripts.js
+++ Day02/Day02-Starter-Code/scripts/scripts.js
@@ -1,18 +1,34 @@
 function validateForm(){
     let empName = document.forms["frmCollectWeights"]["empName"];
     let empWeight = document.forms["frmCollectWeights"]["empWeight"];
+    document.getElementById("nameMessage").innerHTML="";
+    document.getElementById("weightMessage").innerHTML="";
+    let badName = /^([^0-9]*)$/;
     if(empName.value == ""){
         document.getElementById("nameMessage").innerHTML="Name cannot be empty!";
         empName.focus();
         return false;
     }
+    if(!empName.value.match(badName)){
+        document.getElementById("nameMessage").innerHTML="Name cannot be numbvers!";
+        empName.focus();
+        return false;
+    }
+//
     if(empWeight.value == ""){
         document.getElementById("weightMessage").innerHTML="Weight cannot be empty!";
         empWeight.focus();
         return false;
     }
+    if(isNaN(empWeight.value)){
+        document.getElementById("weightMessage").innerHTML="Weight must be a number";
+        empWeight.focus();
+        return false;
+    }
     return true;
+
 }
+//
 let inputTags = document.getElementsByTagName("input");
 for(let i = 0; i < inputTags.length; i++){
     inputTags[i].addEventListener("focus", function(){
